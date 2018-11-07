9c7e97ced0c9b105a33a8c64f4bb7ab6710e69ed
Thanduxolo Tshiba <tntshiba24@gmail.com>
2018-11-07 11:04:30 +0200

    Add files via upload

---
+++ RockPaperScissors.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RockPaperScissors {
+    public static void main(String[] args) {
+        /*
+        1 = rock
+        2 = paper
+        3 = scissors
+        4 = quit
+         */
+        Scanner in = new Scanner(System.in);
+    }
+}
